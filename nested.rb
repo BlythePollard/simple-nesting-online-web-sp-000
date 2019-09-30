dace031db70bc6fcd34cb1167f61cfbe017cdcae
Blythe Pollard <blytheepollard@gmail.com>
2019-09-30 17:17:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,10 +80,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-new_programmer = {:yukihiro_matsumoto => {
+programmer_hash[:yukihiro_matsumoto] = {
    :known_for => "Ruby",
-   :languages => ["LISP", "C"]}}.merge(programmer_hash)
-
+   :languages => ["LISP", "C"]
+}
+programmer_hash
 end
 
 def changing_alan
@@ -104,7 +105,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
        alans_new_info = "GUI"
-      programmer_hash[:alan_kay][:known_for].replace "GUI"
+      programmer_hash[:alan_kay][:known_for] = alans_new_info
       return programmer_hash
      
 end
